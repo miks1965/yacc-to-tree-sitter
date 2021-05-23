40f0609892c0714ece5005e33aeb40623b29d76e
Oxana <lereenadem@yandex.ru>
2021-05-23 17:24:04 +0300

    Save before refactor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,58 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    File("/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/output.txt").writeText("")
-    File("/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/optionals.txt").writeText("")
-    convert(
+//    File("/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/output.txt").writeText("")
+//    File("/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/optionals.txt").writeText("")
+
+//    convert(
+//        "/home/lereena/github/tree-sitter-pascalabcnet/full1904.txt",
+//        "/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/output.txt"
+//    )
+
+    File("/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/withSemanticPrints.txt").writeText("")
+    addSemanticPrintlns(
         "/home/lereena/github/tree-sitter-pascalabcnet/full1904.txt",
-        "/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/output.txt"
+        "/home/lereena/IdeaProjects/yacc-to-tree-sitter/src/withSemanticPrints.txt"
     )
+}
+
+fun addSemanticPrintlns(inputFile: String, outputFile: String) {
+    var content = File(inputFile).readText()
+    var output = StringBuilder()
+
+    val rulesSectionStart = content.indexOf("%%")
+    val rulesSectionEnd = content.lastIndexOf("%%")
+    content = content.substring(rulesSectionStart + 2, rulesSectionEnd)
+
+    val rules = content.split(';')
+    for (rule in rules) {
+        if (rule.trim().isEmpty())
+            continue
+        val splitRule = removeSemanticActions(rule.trim()).split(':')
+        val ruleName = splitRule[0].trim()
+
+        val ruleBranches = splitRule[1].split('|')
+
+        if (ruleBranches.isEmpty())
+            println("Rule $ruleName has no branches")
+
+        val formedRule = if (ruleBranches.size == 1)
+            "$ruleName  : ${ruleBranches[0]} { Console.WriteLine(\"$ruleName.(${ruleBranches[0].trim()})\"); }\n\t;\n"
+        else {
+            val thisRule = StringBuilder()
+            thisRule.append("$ruleName  : ${ruleBranches[0].trim()} { Console.WriteLine(\"$ruleName.(${ruleBranches[0].trim()})\"); }\n")
+            for (i in 1 until ruleBranches.size) {
+                val branch = ruleBranches[i]
+                thisRule.append("\t| ${branch.trim()} { Console.WriteLine(\"$ruleName.(${branch.trim()})\"); }\n")
+            }
+            thisRule.append("\t;\n")
+            thisRule.toString()
+        }
+
+        output.append(formedRule)
+    }
+
+    File(outputFile).writeText(output.toString())
 }
 
 val optionals = mutableListOf<String>()
@@ -19,10 +65,12 @@ fun convert(inputFile: String, outputFile: String) {
     val rulesSectionEnd = content.lastIndexOf("%%")
     content = content.substring(rulesSectionStart + 2, rulesSectionEnd)
 
-    output.append("module.exports = grammar({\n" +
-            "    name: 'pascalabcnet',\n" +
-            "\n" +
-            "    rules: {")
+    output.append(
+        "module.exports = grammar({\n" +
+                "    name: 'pascalabcnet',\n" +
+                "\n" +
+                "    rules: {"
+    )
 
     val rules = content.split(';')
     for (rule in rules) {
